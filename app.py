4a535ecb26f4ca63410a8ea526afa9232c23e578
Lauren Baldino <leb4351@g.rit.edu>
2020-09-05 12:23:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ response = requests.get('https://restcountries.eu/rest/v2/all')
 print(response.status_code)
 parsed_json = json.loads(response.text)
 
-# parsed_countries = []
+parsed_countries = []
 
-# total = 0
-# for country in parsed_json:
-#     # print(country['area'])
-#     if country['area']:
-#         total = total + country['area']
-#     else:
-#         print(country['name'])
+total = 0
+for country in parsed_json:
+    # print(country['area'])
+    if country['area']:
+        total = total + country['area']
+    else:
+        print(country['name'])
 
-# print(total // len(parsed_json))
+print(total // len(parsed_json))
 
 parsed_countries = {}
 
@@ -39,16 +39,16 @@ print(str(parsed_countries))
 
 
 
-# parsed_countries = []
-# for country in parsed_json:
-#     # if country['name'][0] == 'A':
-#     for currency in country['currencies']:
-#         if currency['code'] == 'USD' and country['area'] > 100:
-#             parsed_countries.append(country)
+parsed_countries = []
+for country in parsed_json:
+    # if country['name'][0] == 'A':
+    for currency in country['currencies']:
+        if currency['code'] == 'USD' and country['area'] > 100:
+            parsed_countries.append(country)
 
-# for country in parsed_countries:
-#     print(country['name'] + ':' + str(country['currencies']))
-# print(parsed_countries)
+for country in parsed_countries:
+    print(country['name'] + ':' + str(country['currencies']))
+print(parsed_countries)
 
-# response = requests.post('https://restcountries.eu/rest/v2/submit', data=parsed_countries)
+response = requests.post('https://restcountries.eu/rest/v2/submit', data=parsed_countries)
 
